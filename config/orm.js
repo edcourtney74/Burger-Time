4597a9d853d4251d033f4a1fdb0507612d930113
Ed Courtney <edcourtney74@gmail.com>
2018-12-18 08:41:13 -0500

    Initial ORM functioning.

--- config/orm.js
+++ config/orm.js
@@ -3,16 +3,31 @@ var connection = require("./connection");
 
 // Creating ORM variable to hold MySQL sort functions
 var orm = {
+    // Function to show all burgers in the database
     selectAll: function() {
-
+        connection.query("SELECT * FROM burgers", function (err, data) {
+            if (err) throw err;  
+            console.log(data);
+        });
     },
+    // Function to insert a new burger into the database;
     insertOne: function() {
-
-    },
-    updateOne: function() {
-
+        connection.query("INSERT INTO burgers (burger_name) VALUES (?)", [req.body.burger_name], function(err, result) {
+            if (err) throw err;
+        })
+    },    
+    // Function to update burger's devoured value to true
+    updateOne: function(param) {
+        connection.query("UPDATE burgers SET devoured = true WHERE id = ?", [req.params.id], function (err, result) {
+            if (err) {
+                throw err;
+            // Check to see if a row was changed; if not, throw an error
+            } else if (result.changedRows === 0) {
+                return err;
+            }
+        })
     }
 }
-
+ 
 // * Export the ORM object
 module.exports = orm;
--- server.js
+++ server.js
@@ -1,4 +1,24 @@
 // Bring in express
 var express = require("express");
-
 var app = express();
+
+// Bring in, set up handlebars
+var exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Set the port
+var PORT = process.env.PORT || 8080;
+
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set static folder for assets
+app.use(express.static("public/assets"));
+
+// Start the server
+app.listen(PORT, function() {
+    // Log server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+});
