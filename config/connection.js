74ad6d266ee695cbd48f779e44f0c6587adc6045
Ed Courtney <edcourtney74@gmail.com>
2018-12-19 17:13:48 -0500

    Update connection to work with Heroku

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,7 @@ var connection = mysql.createConnection({
   port: 3306,
   user: "root",
   password: "root",
-  database: "burgers_db"
+  database: "postgres://qzxxpkelypitno:ad2333f961a5ca016b7bb9e960350e564fb0c31235aa2a17c6b3f998cd2aa58b@ec2-54-235-247-209.compute-1.amazonaws.com:5432/d20a5v6sl6k539"
 });
 
 // Export connection
