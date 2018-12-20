c46e2e4b22a3177fb04631a167d71226a98d035b
Ed Courtney <edcourtney74@gmail.com>
2018-12-19 20:29:57 -0500

    Updating database connection for Heroku

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,18 @@
 // Bring in MySQL
 var mysql = require("mysql");
+var connection;
 
-// Connect to database
-var connection = mysql.createConnection({
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "root",
-  database: "postgres://qzxxpkelypitno:ad2333f961a5ca016b7bb9e960350e564fb0c31235aa2a17c6b3f998cd2aa58b@ec2-54-235-247-209.compute-1.amazonaws.com:5432/d20a5v6sl6k539"
-});
+  database: "burgers_db"
+  });
+};
 
 // Export connection
 module.exports = connection;
